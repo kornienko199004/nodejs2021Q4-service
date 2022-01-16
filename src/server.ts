be29e055022710ebe14c554fd1cbd48ce0d45cdb
Kornienko Aleksey <kornienko199004@gmail.com>
2022-01-16 11:38:04 +0300

    feat: add migrations

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
-import { createConnection } from 'typeorm';
+import { ConnectionOptions, createConnection } from 'typeorm';
 import { PORT } from './common/config';
 import app from './app';
 import { logger } from './logger/logger';
+import config from './ormconfig'
 
-createConnection().then(() => {
+createConnection(config as ConnectionOptions).then(async (connection) => {
+  try {
+    await connection.runMigrations();
+    console.log('migration ok');
+  } catch (e) {
+    console.log('migration', e);
+    logger.logError(new Error(`Migration error: ${e}`));
+  }
   app.listen(PORT, () =>
     console.log(`App is running on http://localhost:${PORT}`)
   )
